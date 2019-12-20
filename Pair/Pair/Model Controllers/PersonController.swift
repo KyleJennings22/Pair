4a1d40748034576efaf52d12f7ad335620e7bc6c
Kyle Jennings <Kyle22@gmail.com>
2019-12-20 12:23:22 -0700

    everything works

--- Pair/Pair/Model Controllers/PersonController.swift
+++ Pair/Pair/Model Controllers/PersonController.swift
@@ -12,50 +12,56 @@ class PersonController {
   static let shared = PersonController()
   var people: [Person] = []
   
+  // Loading the current people
   init() {
     loadFromPersistentStore()
   }
   
+  // Creating a person
   func createPerson(name: String) {
     let person = Person(name: name)
     people.append(person)
     saveToPersistentStore()
   }
   
+  // Deleting a person
   func deletePerson(person: Person) {
     guard let index = people.firstIndex(of: person) else {return}
     people.remove(at: index)
     saveToPersistentStore()
   }
   
+  // Creating our file and returning a url of that filepath
   func fileURL() -> URL {
-      let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-      let fileName = "people.json"
-      let documentDirectoryURL = paths[0].appendingPathComponent(fileName)
-      return documentDirectoryURL
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    let fileName = "people.json"
+    let documentDirectoryURL = paths[0].appendingPathComponent(fileName)
+    return documentDirectoryURL
   }
   
+  // Saving using codable
   func saveToPersistentStore() {
-      let encoder = JSONEncoder()
-      
-      do {
-          let data = try encoder.encode(people)
-          try data.write(to: fileURL())
-      } catch {
-          print(error)
-      }
+    let encoder = JSONEncoder()
+    
+    do {
+      let data = try encoder.encode(people)
+      try data.write(to: fileURL())
+    } catch {
+      print(error)
+    }
   }
-
+  
+  // Loading using codable
   func loadFromPersistentStore() {
-      let decoder = JSONDecoder()
-      
-      do {
-          let data = try Data(contentsOf: fileURL())
-          let people = try decoder.decode([Person].self, from: data)
-          self.people = people
-      } catch {
-          print(error)
-      }
+    let decoder = JSONDecoder()
+    
+    do {
+      let data = try Data(contentsOf: fileURL())
+      let people = try decoder.decode([Person].self, from: data)
+      self.people = people
+    } catch {
+      print(error)
+    }
   }
-
+  
 }
